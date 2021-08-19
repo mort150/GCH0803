68d143d139602e93d794f857909aaf6837e050bd
mort150 <thanhdtgch190775@fpt.edu.vn>
2021-08-19 11:11:43 +0700

    ok

--- day 4.1/app.js
+++ day 4.1/app.js
@@ -1,55 +1,108 @@
-const EXPRESS = require('express')
-const {ObjectId,MongoClient} = require('mongodb')
+const EXPRESS = require('express');
+const session = require('express-session');
+const { ObjectId, MongoClient } = require('mongodb')
 
-const {insertStudent,deleteStudent,searchStudent,getAllStudent,getStudentById,updateStudent} = require('./databaseHandler'); 
+const { checkUserRole, insertStudent, deleteStudent, searchStudent, getAllStudent, getStudentById, updateStudent, insertUser } = require('./databaseHandler');
 
 const APP = EXPRESS()
 
+APP.use(session({ secret: '1215665@#53', cookie: { maxAge: 60000 }, saveUninitialized: false, resave: false }))
+APP.use(EXPRESS.urlencoded({ extended: true }))
+APP.set('view engine', 'hbs')
 
-APP.use(EXPRESS.urlencoded({extended:true}))
-APP.set('view engine','hbs')
+APP.get('/login', (req, res) => {
+    res.render('login')
+})
 
-APP.post('/update',async (req,res)=>{
+APP.post('/doLogin', async (req, res) => {
+    const nameInput = req.body.txtName;
+    const passInput = req.body.txtPassword;
+    const userRole = await checkUserRole(nameInput, passInput);
+
+    if (userRole != "-1") {
+        req.session["User"] = {
+            name: nameInput,
+            role: userRole
+        }
+    }
+    res.redirect('/');
+})
+    
+APP.post('/update', async (req, res) => {
     const id = req.body.id;
     const nameInput = req.body.txtName;
     const tuoiInput = req.body.txtTuoi;
-    await updateStudent(id,nameInput,tuoiInput);
+    const pictureInput = req.body.picture;
+    await updateStudent(id, nameInput, tuoiInput, pictureInput);
     res.redirect('/');
 })
 
-APP.get('/edit',async (req,res)=>{
+APP.get('/edit', async (req, res) => {
     const idInput = req.query.id;
     const search_Student = await getStudentById(idInput);
-    res.render('edit',{student:search_Student})
+    res.render('edit', { student: search_Student })
+})
+APP.get('/add', (req, res) => {
+    res.render('detail')
+})
+APP.get('/search', (req, res) => {
+    res.render('searchpage')
 })
 
+APP.get('/home', (req, res) => {
+    res.redirect('/')
+})
 
-APP.post('/insert', async (req,res)=>{
+APP.post('/insert', async (req, res) => {
     const nameInput = req.body.txtName;
     const tuoiInput = req.body.txtTuoi;
-    const newStudent = {name:nameInput,tuoi:tuoiInput};
+    const pictureInput = req.body.picture;
+    const newStudent = { name: nameInput, tuoi: tuoiInput, picture: pictureInput };
     await insertStudent(newStudent);
     //chuyen huong den file index
     res.redirect('/');
 })
 
-APP.get('/delete', async (req,res)=>{
+APP.get('/delete', async (req, res) => {
     const idInput = req.query.id;
     await deleteStudent(idInput);
     res.redirect('/');
 })
 
-APP.post('/search', async (req,res)=>{
+APP.post('/search', async (req, res) => {
     const nameSearch = req.body.txtSearch;
     const allStudents = await searchStudent(nameSearch);
-    res.render('index',{data:allStudents})
+    res.render('index', { data: allStudents })
 })
 
 
-APP.get('/',async(req,res)=>{
+APP.get('/', requireLogin, async (req, res) => {
+    // if(!req.session["User"])
+    //     res.redirect('/login')
     const allStudents = await getAllStudent();
-    res.render('index',{data:allStudents})
+    res.render('index', { data: allStudents, user: req.session["User"] })
 })
+
+APP.get('/nologin', requireLogin, (req, res) => {
+    res.render('noLogin');
+})
+
+APP.post('/register', async (req, res) => {
+    const nameInput = req.body.txtName;
+    const passInput = req.body.txtPassword;
+    const roleInput = req.body.role;
+    await insertUser({ name: nameInput, pass: passInput, role: roleInput })
+    res.redirect('/login')
+})
+
+function requireLogin(req, res, next) {
+    if (req.session["User"]) {
+        return next()
+    } else {
+        res.redirect('/login')
+    }
+
+}
 
 const PORT = process.env.PORT || 5000
 APP.listen(PORT);
--- day 4.1/databaseHandler.js
+++ day 4.1/databaseHandler.js
@@ -1,8 +1,13 @@
-const {MongoClient, ObjectId} = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb')
 
-// const URL ='mongodb://localhost:27017';
-const URL = 'mongodb+srv://mortdz150:mortdz150@cluster0.ltplv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const DATABASE_NAME = "GCH0803DB";
+const URL = 'mongodb://localhost:27017';
+// const URL = 'mongodb+srv://mortdz150:mortdz150@cluster0.ltplv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// const DATABASE_NAME = "GCH0803DB";
+
+async function insertUser(user) {
+    const dbo = await getDB();
+    await dbo.collection("users").insertOne(user);
+}
 
 async function insertStudent(newStudent) {
     const dbo = await getDB();
@@ -11,7 +16,7 @@ async function insertStudent(newStudent) {
 
 async function getDB() {
     const client = await MongoClient.connect(URL);
-    const dbo = client.db("DATABASE_NAME");
+    const dbo = client.db("GCH0803DB");
     return dbo;
 }
 
@@ -33,13 +38,23 @@ async function getAllStudent() {
 }
 
 
-async function getStudentById(idInput){
+async function getStudentById(idInput) {
     const dbo = await getDB();
-    return dbo.collection("students").findOne({_id:ObjectId(idInput)});
+    return dbo.collection("students").findOne({ _id: ObjectId(idInput) });
 }
 
-async function updateStudent(id,nameInput,tuoiInput){
+async function updateStudent(id, nameInput, tuoiInput, pictureInput) {
     const dbo = await getDB();
-    dbo.collection("students").updateOne({_id:ObjectId(id)},{$set:{name:nameInput,tuoi:tuoiInput}})
+    dbo.collection("students").updateOne({ _id: ObjectId(id) }, { $set: { name: nameInput, tuoi: tuoiInput, picture: pictureInput } })
 }
-module.exports = {getDB,insertStudent,deleteStudent,searchStudent,getAllStudent,getStudentById,updateStudent}
+
+async function checkUserRole(nameI, passI) {
+    const dbo = await getDB();
+    const user = await dbo.collection("users").findOne({ name: nameI, pass: passI });
+    if (user == null) {
+        return "-1"
+    } else {
+        return user.role;
+    }
+}
+module.exports = { checkUserRole, getDB, insertStudent, deleteStudent, searchStudent, getAllStudent, getStudentById, updateStudent, insertUser }
